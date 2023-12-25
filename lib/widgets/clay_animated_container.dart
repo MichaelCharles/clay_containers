38044eee91916c2206f14370cd0a57161d8ac546
NonymousMorlock <agamspaul@gmail.com>
2023-12-25 00:44:38 +0000

    Lint fixes and syntax updates

--- lib/widgets/clay_animated_container.dart
+++ lib/widgets/clay_animated_container.dart
@@ -1,8 +1,25 @@
+import 'package:clay_containers/constants.dart';
 import 'package:flutter/material.dart';
 
-import '../constants.dart';
-
 class ClayAnimatedContainer extends StatelessWidget {
+  const ClayAnimatedContainer({
+    super.key,
+    this.child,
+    this.height,
+    this.width,
+    this.color,
+    this.surfaceColor,
+    this.parentColor,
+    this.spread,
+    this.borderRadius,
+    this.customBorderRadius,
+    this.curveType,
+    this.depth,
+    this.emboss,
+    this.duration,
+    this.curve,
+  });
+
   final Duration? duration;
   final Curve? curve;
   final double? height;
@@ -18,51 +35,35 @@ class ClayAnimatedContainer extends StatelessWidget {
   final int? depth;
   final bool? emboss;
 
-  ClayAnimatedContainer(
-      {this.child,
-      this.height,
-      this.width,
-      this.color,
-      this.surfaceColor,
-      this.parentColor,
-      this.spread,
-      this.borderRadius,
-      this.customBorderRadius,
-      this.curveType,
-      this.depth,
-      this.emboss,
-      this.duration,
-      this.curve});
-
-  Color _getAdjustColor(Color baseColor, amount) {
-    Map colors = {
-      "red": baseColor.red,
-      "green": baseColor.green,
-      "blue": baseColor.blue
+  Color _getAdjustColor(Color baseColor, int amount) {
+    var colors = <String, int>{
+      'red': baseColor.red,
+      'green': baseColor.green,
+      'blue': baseColor.blue,
     };
     colors = colors.map((key, value) {
       if (value + amount < 0) return MapEntry(key, 0);
       if (value + amount > 255) return MapEntry(key, 255);
       return MapEntry(key, value + amount);
     });
-    return Color.fromRGBO(colors["red"], colors["green"], colors["blue"], 1);
+    return Color.fromRGBO(colors['red']!, colors['green']!, colors['blue']!, 1);
   }
 
-  List<Color> _getFlatGradients(baseColor, depth) {
+  List<Color> _getFlatGradients(Color baseColor, int depth) {
     return [
       baseColor,
       baseColor,
     ];
   }
 
-  List<Color> _getConcaveGradients(baseColor, depth) {
+  List<Color> _getConcaveGradients(Color baseColor, int depth) {
     return [
       _getAdjustColor(baseColor, 0 - depth),
       _getAdjustColor(baseColor, depth),
     ];
   }
 
-  List<Color> _getConvexGradients(baseColor, depth) {
+  List<Color> _getConvexGradients(Color baseColor, int depth) {
     return [
       _getAdjustColor(baseColor, depth),
       _getAdjustColor(baseColor, 0 - depth),
@@ -71,16 +72,14 @@ class ClayAnimatedContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final double? heightValue = height == null ? null : height;
-    final double? widthValue = width == null ? null : width;
-    final int? depthValue = depth == null ? 20 : depth;
-    Color? colorValue = color == null ? Color(0xFFf0f0f0) : color;
-    final Color parentColorValue =
-        parentColor == null ? colorValue! : parentColor!;
-    final Color? surfaceColorValue =
-        surfaceColor == null ? colorValue : surfaceColor;
-    final double spreadValue = spread == null ? 6 : spread!;
-    final bool embossValue = emboss == null ? false : emboss!;
+    final heightValue = height;
+    final widthValue = width;
+    final depthValue = depth ?? 20;
+    var colorValue = color ?? const Color(0xFFf0f0f0);
+    final parentColorValue = parentColor == null ? colorValue : parentColor!;
+    final surfaceColorValue = surfaceColor ?? colorValue;
+    final spreadValue = spread ?? 6;
+    final embossValue = emboss ?? false;
     BorderRadius? borderRadiusValue = borderRadius == null
         ? BorderRadius.zero
         : BorderRadius.circular(borderRadius!);
@@ -88,54 +87,59 @@ class ClayAnimatedContainer extends StatelessWidget {
     if (customBorderRadius != null) {
       borderRadiusValue = customBorderRadius;
     }
-    final CurveType curveTypeValue =
-        curveType == null ? CurveType.none : curveType!;
+    final curveTypeValue = curveType == null ? CurveType.none : curveType!;
 
-    List<BoxShadow> shadowList = [
+    var shadowList = <BoxShadow>[
       BoxShadow(
-          color: _getAdjustColor(
-              parentColorValue, embossValue ? 0 - depthValue! : depthValue),
-          offset: Offset(0 - spreadValue, 0 - spreadValue),
-          blurRadius: spreadValue),
+        color: _getAdjustColor(
+          parentColorValue,
+          embossValue ? 0 - depthValue : depthValue,
+        ),
+        offset: Offset(0 - spreadValue, 0 - spreadValue),
+        blurRadius: spreadValue,
+      ),
       BoxShadow(
-          color: _getAdjustColor(
-              parentColorValue, embossValue ? depthValue : 0 - depthValue!),
-          offset: Offset(spreadValue, spreadValue),
-          blurRadius: spreadValue)
+        color: _getAdjustColor(
+          parentColorValue,
+          embossValue ? depthValue : 0 - depthValue,
+        ),
+        offset: Offset(spreadValue, spreadValue),
+        blurRadius: spreadValue,
+      ),
     ];
 
     if (embossValue) shadowList = shadowList.reversed.toList();
-    if (embossValue)
-      colorValue = _getAdjustColor(colorValue!, 0 - depthValue! ~/ 2);
+    if (embossValue) {
+      colorValue = _getAdjustColor(colorValue, 0 - depthValue ~/ 2);
+    }
     if (surfaceColor != null) colorValue = surfaceColorValue;
 
     late List<Color?> gradientColors;
     switch (curveTypeValue) {
       case CurveType.concave:
         gradientColors = _getConcaveGradients(colorValue, depthValue);
-        break;
       case CurveType.convex:
         gradientColors = _getConvexGradients(colorValue, depthValue);
-        break;
       case CurveType.none:
         gradientColors = _getFlatGradients(colorValue, depthValue);
-        break;
     }
 
     return AnimatedContainer(
-      duration: this.duration ?? Duration(seconds: 1),
-      curve: this.curve ?? Curves.linear,
+      duration: duration ?? const Duration(seconds: 1),
+      curve: curve ?? Curves.linear,
       height: heightValue,
       width: widthValue,
-      child: child,
       decoration: BoxDecoration(
-          borderRadius: borderRadiusValue,
-          color: colorValue,
-          gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: gradientColors as List<Color>),
-          boxShadow: shadowList),
+        borderRadius: borderRadiusValue,
+        color: colorValue,
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: gradientColors as List<Color>,
+        ),
+        boxShadow: shadowList,
+      ),
+      child: child,
     );
   }
 }
