9d45d89824c91446e1cee53bb0c48c268b20ea52
NonymousMorlock <agamspaul@gmail.com>
2023-12-25 00:58:06 +0000

    Lint fixes and syntax updates

--- lib/widgets/clay_text.dart
+++ lib/widgets/clay_text.dart
@@ -1,6 +1,20 @@
+import 'package:clay_containers/utils/clay_utils.dart';
 import 'package:flutter/material.dart';
 
 class ClayText extends StatelessWidget {
+  const ClayText(
+    this.text, {
+    super.key,
+    this.parentColor,
+    this.textColor,
+    this.color,
+    this.spread,
+    this.depth,
+    this.style,
+    this.size,
+    this.emboss,
+  });
+
   final String text;
   final Color? color;
   final Color? parentColor;
@@ -11,68 +25,54 @@ class ClayText extends StatelessWidget {
   final double? size;
   final bool? emboss;
 
-  ClayText(this.text,
-      {this.parentColor,
-      this.textColor,
-      this.color,
-      this.spread,
-      this.depth,
-      this.style,
-      this.size,
-      this.emboss});
-
-  Color _getAdjustColor(Color baseColor, amount) {
-    Map colors = {
-      "red": baseColor.red,
-      "green": baseColor.green,
-      "blue": baseColor.blue
-    };
-    colors = colors.map((key, value) {
-      if (value + amount < 0) return MapEntry(key, 0);
-      if (value + amount > 255) return MapEntry(key, 255);
-      return MapEntry(key, value + amount);
-    });
-    return Color.fromRGBO(colors["red"], colors["green"], colors["blue"], 1);
-  }
-
-  double? _getSpread(base) {
-    double? calculated = (base / 10).floor().toDouble();
+  double? _getSpread(double base) {
+    final calculated = (base / 10).floor().toDouble();
     return calculated == 0 ? 1 : calculated;
   }
 
   @override
   Widget build(BuildContext context) {
-    final int? depthValue = depth == null ? 40 : depth;
-    Color? colorValue = color == null ? Color(0xFFf0f0f0) : color;
-    final Color outerColorValue =
-        parentColor == null ? colorValue! : parentColor!;
-    double? fontSizeValue = size == null ? 14 : size;
-    final TextStyle styleValue = style == null ? TextStyle() : style!;
-    fontSizeValue =
-        styleValue.fontSize != null ? styleValue.fontSize : fontSizeValue;
-    final double spreadValue =
-        spread == null ? _getSpread(fontSizeValue)! : spread!;
-    final bool embossValue = emboss == null ? false : emboss!;
+    final depthValue = depth ?? 40;
+    var colorValue = color ?? const Color(0xFFf0f0f0);
+    final outerColorValue = parentColor ?? colorValue;
+    var fontSizeValue = size ?? 14;
+    final styleValue = style ?? const TextStyle();
+    fontSizeValue = styleValue.fontSize ?? fontSizeValue;
+    final spreadValue = spread ?? _getSpread(fontSizeValue)!;
+    final embossValue = emboss ?? false;
 
-    List<Shadow> shadowList = [
+    var shadowList = <Shadow>[
       Shadow(
-          color: _getAdjustColor(
-              outerColorValue, embossValue ? 0 - depthValue! : depthValue),
-          offset: Offset(0 - spreadValue / 2, 0 - spreadValue / 2),
-          blurRadius: spreadValue),
+        color: ClayUtils.getAdjustColor(
+          outerColorValue,
+          embossValue ? 0 - depthValue : depthValue,
+        ),
+        offset: Offset(0 - spreadValue / 2, 0 - spreadValue / 2),
+        blurRadius: spreadValue,
+      ),
       Shadow(
-          color: _getAdjustColor(
-              outerColorValue, embossValue ? depthValue : 0 - depthValue!),
-          offset: Offset(spreadValue / 2, spreadValue / 2),
-          blurRadius: spreadValue)
+        color: ClayUtils.getAdjustColor(
+          outerColorValue,
+          embossValue ? depthValue : 0 - depthValue,
+        ),
+        offset: Offset(spreadValue / 2, spreadValue / 2),
+        blurRadius: spreadValue,
+      ),
     ];
 
     if (embossValue) shadowList = shadowList.reversed.toList();
-    if (embossValue) colorValue = _getAdjustColor(colorValue!, 0 - depthValue!);
-    if (textColor != null) colorValue = textColor;
+    if (embossValue) {
+      colorValue = ClayUtils.getAdjustColor(colorValue, 0 - depthValue);
+    }
+    if (textColor != null) colorValue = textColor!;
 
-    return Text(text,
-        style: styleValue.copyWith(
-            color: colorValue, shadows: shadowList, fontSize: fontSizeValue));
+    return Text(
+      text,
+      style: styleValue.copyWith(
+        color: colorValue,
+        shadows: shadowList,
+        fontSize: fontSizeValue,
+      ),
+    );
   }
 }
